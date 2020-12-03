806465af6b6008bd6cfebaf16c065b221963da5e
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-12-02 22:37:07 -0300

    test: obter playlist a partir das coordenadas

--- __tests__/suggestion.route.spec.ts
+++ __tests__/suggestion.route.spec.ts
@@ -1,4 +1,5 @@
 import cities from 'all-the-cities';
+import ramdomCoordinate from 'random-coordinates';
 import request from 'supertest';
 
 import SpotifyAPI from '../src/api/spotify.api';
@@ -7,11 +8,30 @@ import app from '../src/start/app';
 describe('suggestion route', () => {
   beforeAll(async () => await new SpotifyAPI().authenticate());
 
-  test('GET /suggestion should return properties "tracks" and "playlist"', async () => {
+  test('GET /suggestion receive city name, should return properties "tracks" and "playlist"', async () => {
     const city = cities[Math.floor(Math.random() * cities.length)].name;
 
     const response = await request(app).get('/suggestion').query({
       cityName: city,
+    });
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('tracks');
+    expect(response.body).toHaveProperty('playlist');
+  });
+
+  test('GET /suggestion receive coordinates, should return properties "tracks" and "playlist"', async () => {
+    const coordinates: Array<string> = ramdomCoordinate()
+      .replace(' ', '')
+      .split(',');
+    const lat = coordinates[0];
+    const lon = coordinates[1];
+
+    console.log(coordinates);
+
+    const response = await request(app).get('/suggestion').query({
+      lat: lat,
+      lon: lon,
     });
 
     expect(response.status).toBe(200);
